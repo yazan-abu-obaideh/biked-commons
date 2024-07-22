55e0d6b2865617cc403a6cac2b88a674f0be1fce
Yazan Amer <yazan.amer@protonmail.com>
2024-07-22 12:32:26 +0200

    feat: xml handling

---
+++ test/test_rendering.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class RenderingTest(unittest.TestCase):
+    def test_render_biked(self):
+        pass
+
+    def test_render_clip(self):
+        pass
+
+    def test_render_bike_xml_file(self):
+        pass
