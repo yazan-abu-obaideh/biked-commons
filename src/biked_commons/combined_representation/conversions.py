749de7fecf1b330dc3fe9814205fa0e2c1684297
Yazan Amer <yazan.amer@protonmail.com>
2025-03-08 20:55:45 +0300

    feat: better conversions

--- src/biked_commons/combined_representation/conversions.py
+++ src/biked_commons/combined_representation/conversions.py
@@ -1,7 +1,51 @@
+import abc
 from typing import Callable
 
 import attrs
 import pandas as pd
+
+
+class ReversibleConversion(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def apply(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abc.abstractmethod
+    def reverse(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+
+class RenameColumn(ReversibleConversion):
+    def __init__(self, from_name: str, to_name: str):
+        self._from = from_name
+        self._to = to_name
+
+    def apply(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        return dataframe.rename(columns={
+            self._from: self._to
+        })
+
+    def reverse(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        return dataframe.rename(columns={
+            self._to: self._from
+        })
+
+
+class ScaleColumn(ReversibleConversion):
+    def __init__(self, column: str, multiplier: float):
+        self._multiplier = multiplier
+        self._column = column
+
+    def apply(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        return dataframe.assign(**{
+            self._column: dataframe[self._column] * self._multiplier
+        })
+
+    def reverse(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        return dataframe.assign(
+            **{
+                self._column: dataframe[self._column] / self._multiplier
+            })
 
 
 @attrs.define(frozen=True)
---
+++ test/test_conversions.py
@@ -0,0 +1,49 @@
+import unittest
+
+import numpy.testing as np_test
+import pandas as pd
+
+from biked_commons.combined_representation.conversions import RenameColumn, ReversibleConversion, ScaleColumn
+
+
+class ReversibleConversionsTest(unittest.TestCase):
+    def test_rename_column(self):
+        data = pd.DataFrame.from_records([{"A": i} for i in range(10)])
+        expected_converted = pd.DataFrame.from_records([{"B": i} for i in range(10)])
+        conversion = RenameColumn("A", "B")
+        self.assert_conversion(
+            starting_data=data,
+            conversion=conversion,
+            expected_converted=expected_converted
+        )
+
+    def test_scale_column(self):
+        data = pd.DataFrame.from_records([{"A": i} for i in range(10)])
+        expected_converted = pd.DataFrame.from_records([{"A": i * 1000} for i in range(10)])
+        conversion = ScaleColumn("A", 1000)
+        self.assert_conversion(
+            starting_data=data,
+            expected_converted=expected_converted,
+            conversion=conversion
+        )
+
+    def assert_conversion(self,
+                          starting_data: pd.DataFrame,
+                          conversion: ReversibleConversion,
+                          expected_converted: pd.DataFrame):
+        starting_copy = starting_data.copy(deep=True)
+        actual_converted = conversion.apply(starting_data)
+
+        self.assert_dataframes_equal(actual_converted, expected_converted)
+        self.assert_original_not_modified(starting_copy, starting_data)
+        self.assert_dataframes_equal(conversion.reverse(actual_converted), starting_copy)
+
+    def assert_original_not_modified(self, starting_copy: pd.DataFrame, starting_data: pd.DataFrame):
+        self.assert_dataframes_equal(starting_copy, starting_data)
+
+    def assert_dataframes_equal(self,
+                                first: pd.DataFrame,
+                                second: pd.DataFrame):
+        self.assertEqual(list(first.columns), list(second.columns))
+        np_test.assert_array_equal(first.index.values, second.index.values)
+        np_test.assert_array_equal(first.values, second.values)
