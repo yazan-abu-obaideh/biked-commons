4627122af5b16e5ca7ed927ea48950092f3e536c
Yazan Amer <yazan.amer@protonmail.com>
2025-03-25 16:23:09 +0300

    fix: windows rendering

--- src/biked_commons/rendering/bikeCad_renderer.py
+++ src/biked_commons/rendering/bikeCad_renderer.py
@@ -122,11 +122,13 @@ class BikeCad:
         return process
 
     def kill(self):
-        self._instance.kill()
+        self._instance.terminate()
+        self._event_loop.run_until_complete(self._instance.wait())
 
     def _run(self, command):
         self._log_info(f"Running command {command}...")
         self._instance.stdin.write(bytes(command, 'UTF-8'))
+        self._instance.wait()
         self._await_termination()
 
     def _await_termination(self):
