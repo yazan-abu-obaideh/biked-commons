1687356003b146890c8aa07e1497e1f8595e0c35
Yazan Amer Abu Obaideh <yazan.amer@protonmail.com>
2025-03-28 18:36:15 +0100

    Fix rendering on Windows (#3)
    Fix rendering on Windows by using a BikeCAD server jar instead of a BikeCAD console jar

--- test/test_rendering.py
+++ test/test_rendering.py
@@ -1,5 +1,6 @@
 import json
 import unittest
+import uuid
 from difflib import SequenceMatcher
 
 from biked_commons.api.rendering import SingleThreadedRenderer
@@ -25,20 +26,28 @@ class RenderingTest(unittest.TestCase):
 
     def test_render_biked(self):
         actual_result = self.renderer.render_biked(SAMPLE_BIKE_OBJECT)
+        with open(f"some_file_{uuid.uuid4()}", "wb") as file:
+            file.write(actual_result.image_bytes)
         self.assertImagesEqual(actual_result.image_bytes, "expected_render_biked.svg")
 
     def test_render_bike_xml_file(self):
         actual_result = self.renderer.render_xml(self.standard_bike_xml)
+        with open(f"some_file_{uuid.uuid4()}", "wb") as file:
+            file.write(actual_result.image_bytes)
         self.assertImagesEqual(actual_result.image_bytes, "expected_standard_bike_img.svg")
 
     def test_render_clip(self):
         actual_result = self.renderer.render_clip(SAMPLE_CLIPS_OBJECT)
+        with open(f"some_file_{uuid.uuid4()}", "wb") as file:
+            file.write(actual_result.image_bytes)
         self.assertImagesEqual(actual_result.image_bytes, "expected_clips_bike_img.svg")
 
     def assertImagesEqual(self, rendering_result: bytes, test_image_path: str):
         try:
             with open(path_of_test_resource(test_image_path), "r") as image_file:
                 result_str = str(rendering_result, "utf-8")
+                len_res = len(result_str)
+                print(f"f{len_res=}")
                 expected_str = image_file.read()
                 exactly_equal = result_str == expected_str
                 similar_enough = int(exactly_equal)
