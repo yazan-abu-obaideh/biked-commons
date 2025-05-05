fix gradient issues with clipping + attempt to retrain usability

src/biked_commons/design_evaluation/design_evaluation.py
@@ -351,7 +351,7 @@ class UsabilityEvaluator(EvaluationFunction):
 
     def evaluate(self, designs: torch.Tensor, conditioning: dict = {}) -> torch.Tensor:
         if self.target_type == 'cont':
-            preds = self.model.predict(designs.to(self.device, dtype=self.dtype))
+            preds = self.model.predict(designs)
             return torch.clip(preds, min=0, max=1)
         elif self.target_type == 'binary':
             x_input = designs.detach().cpu().numpy()

src/biked_commons/design_evaluation/scoring.py
@@ -24,12 +24,15 @@ class ScoringFunction(ABC):
     def evaluate(self, designs: torch.Tensor, conditioning: dict = {}) -> torch.Tensor:
         pass
 
-def compute_ref_point(ref_scores):
+def compute_ref_point(ref_scores, reduction):
     ref_scores[np.isnan(ref_scores)] = -float("inf")
-    ref_point = np.max(ref_scores, axis=0)
+    if reduction == "max":
+        ref_point = np.max(ref_scores, axis=0)
+    elif reduction == "meanabs":
+        ref_point = np.mean(np.abs(ref_scores), axis=0)
     return ref_point
 
-def recompute_ref_point(evaluator, eval_names, path):
+def recompute_ref_point(evaluator, eval_names, path, reduction):
     print("Calculating reference point for scoring functions...")
     data = pd.read_csv(split_datasets_path("bike_bench.csv"), index_col=0)
     num_data = data.shape[0]
@@ -41,21 +44,26 @@ def recompute_ref_point(evaluator, eval_names, path):
 
     scores = evaluator(torch.tensor(data.values, dtype=torch.float32), condition)
     objective_scores = scores.detach().numpy()
-    ref_point = compute_ref_point(objective_scores)
+    ref_point = compute_ref_point(objective_scores, reduction)
     df = pd.Series(ref_point, index=eval_names)
     df.to_csv(path, header=False)
     return df
 
-def get_ref_point(evaluator, objective_names, eval_names):
-    path = resource_path("misc/ref_point.csv")
+def get_ref_point(evaluator, objective_names, eval_names, reduction = "max"):
+    if reduction=="max":
+        path = resource_path("misc/ref_point.csv")
+    elif reduction=="meanabs":
+        path = resource_path("misc/default_weights.csv")
+    else:
+        raise ValueError("Invalid reduction method. Use 'max' or 'meanabs'.")
     if not os.path.exists(path):
-        ref_point_df = recompute_ref_point(evaluator, eval_names, path)
+        ref_point_df = recompute_ref_point(evaluator, eval_names, path, reduction)
     else:
         ref_point_df = pd.read_csv(path, index_col=0, header=None)
         ref_point_columns = ref_point_df.index.values
         if not np.all(np.isin(objective_names, ref_point_columns)):
             print("Reference point does not include all objective names. Recomputing...")
-            ref_point_df = recompute_ref_point(evaluator, eval_names, path)
+            ref_point_df = recompute_ref_point(evaluator, eval_names, path, reduction)
     ref_point_df = ref_point_df.loc[objective_names]
     ref_point = ref_point_df.values.flatten()
     return ref_point

src/biked_commons/ergonomics/joint_angles.py
@@ -93,7 +93,7 @@ def validity_mask(bikes, bodies, arm_angle):
 ###################################
 # FUNCTIONS FOR CALCUATING ANGLES #
 ###################################
-def knee_extension_angle(bike_vectors, body_vectors, CA, ret_a2=False):
+def knee_extension_angle(bike_vectors, body_vectors, CA, ret_a2=False, eps=1e-6):
     """
     Input:
         bike vector, body vector, crank angle, OPTIONAL return a2
@@ -146,7 +146,7 @@ def knee_extension_angle(bike_vectors, body_vectors, CA, ret_a2=False):
     LY = SY - CL * torch.sin(CA)
     x_2 = torch.sqrt(LX ** 2 + LY ** 2)
 
-    alpha_1 = torch.arccos(torch.clamp((x_1 ** 2 - UL_s - x_2 ** 2) / (-2 * UL * x_2), -1.0, 1.0))
+    alpha_1 = torch.arccos(torch.clamp((x_1 ** 2 - UL_s - x_2 ** 2) / (-2 * UL * x_2), -1.0 + eps, 1.0 - eps))
     alpha_2 = torch.atan2(LY, LX) - alpha_1
     if ret_a2:
         return alpha_2
@@ -155,14 +155,14 @@ def knee_extension_angle(bike_vectors, body_vectors, CA, ret_a2=False):
     LLX = LX - UL * torch.cos(alpha_2)
 
     alpha_3 = torch.atan2(LLY, LLX) - alpha_2
-    alpha_4 = torch.arccos(torch.clamp((FL_s - LL_s - x_1 ** 2) / (-2 * LL * x_1), -1.0, 1.0))
+    alpha_4 = torch.arccos(torch.clamp((FL_s - LL_s - x_1 ** 2) / (-2 * LL * x_1), -1.0 + eps, 1.0 - eps))
 
     return (alpha_3 + alpha_4) * (180 / torch.pi)
 
 def law_of_cosines(a, b, c):
     return (a**2 + b**2 - c**2) / (2 * a * b)
 
-def back_armpit_angles(bike_vectors, body_vectors):
+def back_armpit_angles(bike_vectors, body_vectors, eps=1e-6):
     """
     Input: bike_vector, body_vector, elbow_angle in degrees
     Output: back angle, armpit to elbow angle, armpit to wrist angle in degrees
@@ -197,11 +197,11 @@ def back_armpit_angles(bike_vectors, body_vectors):
 
     tors_angle_cos = law_of_cosines(sth_dist, TL, shoulder_to_hand)
 
-    tors_angle_clamped = torch.clamp(tors_angle_cos, -1.0, 1.0)
+    tors_angle_clamped = torch.clamp(tors_angle_cos, -1.0 + eps, 1.0 - eps)
     tors_ang = torch.arccos(tors_angle_clamped)
 
     shoulder_angle_cos = law_of_cosines(shoulder_to_hand, TL, sth_dist)
-    shoulder_angle_clamped = torch.clamp(shoulder_angle_cos, -1.0, 1.0)
+    shoulder_angle_clamped = torch.clamp(shoulder_angle_cos, -1.0 + eps, 1.0 - eps)
     shoulder_ang = torch.arccos(shoulder_angle_clamped)
 
     #if less than one set angle to 180 degrees and add shoulder_to_hand - sth_dist - TL to give gradient

src/biked_commons/transformation/interface_points.py
@@ -19,7 +19,7 @@ def calculate_interace_points_df(df):
     return y
 
 
-def calculate_interface_points(x, dtype=torch.float32):
+def calculate_interface_points(x, dtype=torch.float32, eps=1e-6):
     stack = x[:, 0]
     oh0 = x[:, 1]  # Handlebar style OHCLASS: 0
     oh1 = x[:, 2]  # Handlebar style OHCLASS: 1
@@ -39,7 +39,7 @@ def calculate_interface_points(x, dtype=torch.float32):
     HTA = x[:, 8] * math.pi / 180 # Head tube angle
     DTL = x[:, 9]  # DT Length
     DTJY = stack - (HTL - HTLX) * torch.sin(HTA)
-    DTJX = torch.sqrt(torch.clip(DTL ** 2 - DTJY ** 2, min=0))
+    DTJX = torch.sqrt(torch.clip(DTL ** 2 - DTJY ** 2, min=eps))
     handlebar_mount_x = DTJX - (HTL - HTLX) * torch.cos(HTA)
     handlebar_mount_y = stack
 
@@ -103,4 +103,5 @@ def calculate_interface_points(x, dtype=torch.float32):
     #offsets from shoe thickness (from bikeCAD)
     hip_y = hip_y - 23
     hand_y = hand_y - 23
+
     return y

src/biked_commons/validation/clip_validation_functions.py
@@ -137,12 +137,12 @@ class SeatStaySmallerThanWheelRadius(ValidationFunction):
     def variable_names(self) -> List[str]:
         return ["CS textfield", "BB textfield","Seat tube length", "Seat stay junction0", "Seat angle", "Wheel diameter rear"]
 
-    def validate(self, designs: torch.tensor) -> torch.tensor:
+    def validate(self, designs: torch.tensor, eps = 1e-6) -> torch.tensor:
         CS_textfield, BB_textfield, Seat_tube_length, Seat_stay_junction0, Seat_angle, RD = designs[:, :len(self.variable_names())].T
         Seat_angle_rad = (Seat_angle * math.pi) / 180
         x = Seat_tube_length-(BB_textfield/torch.sin(Seat_angle_rad))-Seat_stay_junction0
         y = BB_textfield/torch.tan(Seat_angle_rad)
-        z = torch.sqrt(torch.clip((CS_textfield ** 2)-(BB_textfield ** 2), min=0))
+        z = torch.sqrt(torch.clip((CS_textfield ** 2)-(BB_textfield ** 2), min=eps))
         h = z-y
         g = torch.sqrt(h**2 + x**2 - 2*h*x*torch.cos(Seat_angle_rad))
         return (RD / 2) - g
@@ -174,7 +174,7 @@ class PedalIntersectsFrontWheel(ValidationFunction):
     def variable_names(self) -> List[str]:
         return ["Stack", "Head tube length textfield", "Head tube lower extension2", "Head angle", "BB textfield", "DT Length", "FORK0R", "Wheel diameter rear", "Wheel diameter front"]
 
-    def validate(self, designs: torch.tensor) -> torch.tensor:
+    def validate(self, designs: torch.tensor, eps=1e-6) -> torch.tensor:
         Stack, Head_tube_length_textfield, Head_tube_lower_extension2, Head_angle, BB_textfield, DT_length, fork0r, WDR, WDF = designs[:, :len(self.variable_names())].T
         # Extract variables from the DataFrame
         HTL = Head_tube_length_textfield
@@ -187,7 +187,7 @@ class PedalIntersectsFrontWheel(ValidationFunction):
         # Calculate DTJY and DTJX
         DTJY = Stack - (HTL - HTLX) * torch.sin(HTA)
 
-        DTJX = torch.sqrt(torch.clip(DTL ** 2 - DTJY ** 2, min=0))
+        DTJX = torch.sqrt(torch.clip(DTL ** 2 - DTJY ** 2, min=eps))
 
         # Calculate FWX and FCD
         FWX = DTJX + (DTJY - BBD) / torch.tan(HTA)
@@ -213,7 +213,7 @@ class CrankHitsGroundInLowestPosition(ValidationFunction):
         crank_length = 172.5
         return  (crank_length + BB_textfield) - wheel_radius
 
-class RGBvalueGreaterThan255(ValidationFunction):
+class RGBvalueGreaterThan255(ValidationFunction): #less than 0 covered in PositiveValueNegative
     def friendly_name(self) -> str:
         return "RGB value should be less than 255"
 
@@ -223,7 +223,10 @@ class RGBvalueGreaterThan255(ValidationFunction):
     def validate(self, designs: torch.tensor) -> torch.tensor:
         color_overflow = designs - 255
         overflow_clipped = torch.clip(color_overflow, min=0)
-        return torch.sum(overflow_clipped, dim=1)
+        total = torch.sum(overflow_clipped, dim=1)
+        #return total if total > 0 else sum of color_overflow (required for calculation of default weights)
+        mask = total > 0
+        return total * mask.float() + color_overflow.sum(dim=1) * (1 - mask.float())
 
 
 